c2ee54f785c8f459d9e5ac42e75c74a95f800612
Arik Talmon <Ariktalmon@students.berkeley.net>
2017-02-14 11:34:02 -0800

    commit

--- code.js
+++ code.js
@@ -12,111 +12,111 @@ function State(id, name, coast, politics, weather, bigcity, diversity){
     stateArray.push(this);
 }
 
-var alabama = new State(1, "alabama", true, "conservative", "hot", true, "medium");
+var alabama = new State(0, "alabama", true, "conservative", "hot", true, "medium");
 
-var alaska = new State(2, "alaska", true, "conservative", "cold", false, "medium");
+var alaska = new State(1, "alaska", true, "conservative", "cold", false, "medium");
 
-var arizona = new State(3, "arizona", false, "conservative", "hot", true, "low");
+var arizona = new State(2, "arizona", false, "conservative", "hot", true, "low");
 
-var arkansas = new State(4, "arkansas", false, "conservative", "varied", false, "low");
+var arkansas = new State(3, "arkansas", false, "conservative", "varied", false, "low");
 
-var california = new State(5, "california", true, "liberal", "varied", true, "high");
+var california = new State(4, "california", true, "liberal", "varied", true, "high");
 
-var colorado = new State(6, "colorado", false, "swing", "varied", true, "low");
+var colorado = new State(5, "colorado", false, "swing", "varied", true, "low");
 
-var connecticut = new State(7, "connecticut", true, "liberal", "varied", false, "low");
+var connecticut = new State(6, "connecticut", true, "liberal", "varied", false, "low");
 
-var delaware = new State(8, "delaware", true, "swing", "varied", false, "low");
+var delaware = new State(7, "delaware", true, "swing", "varied", false, "low");
 
-var florida = new State(9," florida", true, "swing", "hot", true, "high");
+var florida = new State(8," florida", true, "swing", "hot", true, "high");
 
-var  georgia = new State(10, "georgia", true, "conservative", "hot", true, "high");
+var  georgia = new State(9, "georgia", true, "conservative", "hot", true, "high");
 
-var hawaii = new State(11, "hawaii", true, "liberal", "hot", true, "high");
+var hawaii = new State(10, "hawaii", true, "liberal", "hot", true, "high");
 
-var idaho = new State(12, "idaho", false, "concervative", "varied", false, "low");
+var idaho = new State(11, "idaho", false, "concervative", "varied", false, "low");
 
-var illinois = new State(13, "illinois", false, "liberal", "varied", true, "low");
+var illinois = new State(12, "illinois", false, "liberal", "varied", true, "low");
 
-var indiana = new State(14, "indiana", false, "conservative", "varied", true, "low");
+var indiana = new State(13, "indiana", false, "conservative", "varied", true, "low");
 
-var iowa = new State(15, "iowa", false, "swing", "varied", false, "low");
+var iowa = new State(14, "iowa", false, "swing", "varied", false, "low");
 
-var kansas = new State(16, "kansas", false, "conservative", "hot", false, "low");
+var kansas = new State(15, "kansas", false, "conservative", "hot", false, "low");
 
-var kentucky = new State(17, "kentucky", false, "conservative", "varied", false, "low");
+var kentucky = new State(16, "kentucky", false, "conservative", "varied", false, "low");
 
-var louisiana = new State(18, "louisiana", true, "conservative", "hot", true, "high");
+var louisiana = new State(17, "louisiana", true, "conservative", "hot", true, "high");
 
-var maine = new State(19, "maine", true, "liberal", "cold", false, "low");
+var maine = new State(18, "maine", true, "liberal", "cold", false, "low");
 
-var maryland = new State(20, "maryland", true, "liberal", "varied", true, "medium");
+var maryland = new State(19, "maryland", true, "liberal", "varied", true, "medium");
 
-var massachusetts = new State(21, "massachusetts", true, "liberal", "varied", true, "low");
+var massachusetts = new State(20, "massachusetts", true, "liberal", "varied", true, "low");
 
-var michigan = new State(22, "michigan", false, "swing", "cold", true, "low");
+var michigan = new State(21, "michigan", false, "swing", "cold", true, "low");
 
-var minnesota = new State(23, "minnesota", false, "liberal", "cold", true, "low");
+var minnesota = new State(22, "minnesota", false, "liberal", "cold", true, "low");
 
-var mississippi = new State(24, "mississippi", true, "conservative", "hot", false, "high");
+var mississippi = new State(23, "mississippi", true, "conservative", "hot", false, "high");
 
-var missouri = new State(25, "missouri", false, "conservative", "varied", true, "low");
+var missouri = new State(24, "missouri", false, "conservative", "varied", true, "low");
 
-var montana = new State(26, "montana", false, "conservative", "cold", false, "low");
+var montana = new State(25, "montana", false, "conservative", "cold", false, "low");
 
-var nebraska = new State(27, "nebraska", false, "conservative", "varied", false, "low");
+var nebraska = new State(26, "nebraska", false, "conservative", "varied", false, "low");
 
-var nevada = new State(28, "nevada", false, "swing", "hot", true, "high");
+var nevada = new State(27, "nevada", false, "swing", "hot", true, "high");
 
-var newHampshire = new State(29, "new hampshire", true, "swing", "cold", false, "low");
+var newHampshire = new State(28, "new hampshire", true, "swing", "cold", false, "low");
 
-var newJersey = new State(30, "new jersey", true, "swing", "varied", false, "high");
+var newJersey = new State(29, "new jersey", true, "swing", "varied", false, "high");
 
-var newMexico = new State(31, "new mexico", false, "swing", "hot", true, "high");
+var newMexico = new State(30, "new mexico", false, "swing", "hot", true, "high");
 
-var newYork = new State(32, "new york", true, "swing", "varied", true, "high");
+var newYork = new State(31, "new york", true, "swing", "varied", true, "high");
 
-var northCarolina = new State(33, "north carolina", true, "conservative", "hot", true, "medium");
+var northCarolina = new State(32, "north carolina", true, "conservative", "hot", true, "medium");
 
-var northDakota = new State(34, "north dakota", false, "conservative", "cold", false, "low");
+var northDakota = new State(33, "north dakota", false, "conservative", "cold", false, "low");
 
-var ohio = new State(35, "ohio", false, "swing", "varied", true, "low");
+var ohio = new State(34, "ohio", false, "swing", "varied", true, "low");
 
-var oklahoma = new State(36, "oklahoma", false, "conservative", "varied", false, "low");
+var oklahoma = new State(35, "oklahoma", false, "conservative", "varied", false, "low");
 
-var oregon = new State(37, "oregon", true, "liberal", "cold", true, "low");
+var oregon = new State(36, "oregon", true, "liberal", "cold", true, "low");
 
-var pennsylvania = new State(38, "pennsylvania", false, "swing", "varied", true, "low");
+var pennsylvania = new State(37, "pennsylvania", false, "swing", "varied", true, "low");
 
-var rhodeIsland = new State(39, "rhode island", true, "liberal", "varied", false, "medium");
+var rhodeIsland = new State(38, "rhode island", true, "liberal", "varied", false, "medium");
 
-var southCarolina = new State(40, "south carolina", true, "conservative", "hot", false, "high");
+var southCarolina = new State(39, "south carolina", true, "conservative", "hot", false, "high");
 
-var southDakota = new State(41, "south dakota", false, "conservative", "cold", false, "low");
+var southDakota = new State(40, "south dakota", false, "conservative", "cold", false, "low");
 
-var tennessee = new State(42, "tennessee", false, "conservative", "varied", true, "low");
+var tennessee = new State(41, "tennessee", false, "conservative", "varied", true, "low");
 
-var texas = new State(43, "texas", true, "conservative", "hot", true, "high");
+var texas = new State(42, "texas", true, "conservative", "hot", true, "high");
 
-var utah = new State(44, "utah", false, "conservative", "hot", true, "low");
+var utah = new State(43, "utah", false, "conservative", "hot", true, "low");
 
-var vermont = new State(45, "vermont", false, "liberal", "cold", false, "low");
+var vermont = new State(44, "vermont", false, "liberal", "cold", false, "low");
 
-var virginia = new State(46, "virginia", true, "swing", "varied", true, "medium");
+var virginia = new State(45, "virginia", true, "swing", "varied", true, "medium");
 
-var washington = new State(47, "washington", true, "liberal", "cold", true, "low");
+var washington = new State(46, "washington", true, "liberal", "cold", true, "low");
 
-var westVirginia = new State(48, "west virginia", false, "conservative", "varied", false, "low");
+var westVirginia = new State(47, "west virginia", false, "conservative", "varied", false, "low");
 
-var wisconsin = new State(49, "wisconsin", false, "swing", "cold", true, "low");
+var wisconsin = new State(48, "wisconsin", false, "swing", "cold", true, "low");
 
-var wyoming = new State(50, "wyoming", false, "conservative", "cold", false, "low");
+var wyoming = new State(49, "wyoming", false, "conservative", "cold", false, "low");
 
-var washingtonDC = new State(51, "washington D.D.", false, "liberal", "varied", true, "high");
+var washingtonDC = new State(50, "washington D.D.", false, "liberal", "varied", true, "high");
 
-var puertoRico = new State(52, "puerto rico", true, "apolitical", "hot", false, "high");
+var puertoRico = new State(51, "puerto rico", true, "apolitical", "hot", false, "high");
 
-var guam = new State(53, "guam", true, "apolitical", "hot", false, "high");
+var guam = new State(52, "guam", true, "apolitical", "hot", false, "high");
 
 console.log(stateArray);
 
--- code2.js
+++ code2.js
@@ -2,19 +2,42 @@
 
 function run() {
 
+    var possibleArray = [];
+    var sw = false;
+
     var coast = document.getElementById("coast").value;
+        if (coast == "false") {
+            coast = (coast == "false");
+        } else {
+            coast = (coast == "true");
+        }
     var pol = document.getElementById("politics").value;
     var weather = document.getElementById("weather").value;
     var city = document.getElementById("bigcity").value;
+        if (city == "false") {
+            city = (city == "false");
+        } else {
+            city = (city == "true");
+        }
     var diver = document.getElementById("diversity").value;
 
+    for (var i = 0; i < stateArray.length; i++) {
 
-    for(var i = 0; i < stateArray.length; i++) {
-
-        if (stateArray[i].coast) {
+        if (stateArray[i].coast == coast && stateArray[i].politics == pol && stateArray[i].weather == weather && stateArray[i].bigcity == city
+            && stateArray[i].diversity == diver) {
             console.log(stateArray[i].name);
+        } else {
+            sw = true;
+        }
+
+        if (sw = true) {
+            if (stateArray[i].coast == coast && stateArray[i].weather == weather &&
+                stateArray[i].bigcity == city) {
+                console.log(stateArray[i].name);
+            }
         }
 
     }
+    console.log(possibleArray);
 
 }
