b32779f60c2e216f7db9514284bd1f8d3b41e12b
Arik Talmon <Ariktalmon@students.berkeley.net>
2017-02-15 11:26:13 -0800

    commit

--- code2.js
+++ code2.js
@@ -1,43 +1,37 @@
 
-
 function run() {
 
+    //use myVars instead?
     var possibleArray = [];
-    var sw = false;
 
     var coast = document.getElementById("coast").value;
-        if (coast == "false") {
-            coast = (coast == "false");
-        } else {
-            coast = (coast == "true");
-        }
+    if (coast == "false") {
+        coast = (coast == "false");
+    } else {
+        coast = (coast == "true");
+    }
     var pol = document.getElementById("politics").value;
     var weather = document.getElementById("weather").value;
     var city = document.getElementById("bigcity").value;
-        if (city == "false") {
-            city = (city == "false");
-        } else {
-            city = (city == "true");
-        }
+    if (city == "false") {
+        city = (city == "false");
+    } else {
+        city = (city == "true");
+    }
     var diver = document.getElementById("diversity").value;
 
     for (var i = 0; i < stateArray.length; i++) {
 
-        if (stateArray[i].coast == coast && stateArray[i].politics == pol && stateArray[i].weather == weather && stateArray[i].bigcity == city
-            && stateArray[i].diversity == diver) {
-            console.log(stateArray[i].name);
-        } else {
-            sw = true;
-        }
-
-        if (sw = true) {
-            if (stateArray[i].coast == coast && stateArray[i].weather == weather &&
-                stateArray[i].bigcity == city) {
-                console.log(stateArray[i].name);
-            }
+        if (stateArray[i].coast == coast && stateArray[i].politics == pol && stateArray[i].weather == weather && stateArray[i].bigcity == city && stateArray[i].diversity == diver) {
+            possibleArray = possibleArray + stateArray[i].name;
         }
 
     }
-    console.log(possibleArray);
+
+    if (possibleArray.length == 0) {
+        console.log("Im sorry, but we came up with no results that match your specifications.")
+    } else {
+        console.log(possibleArray);
+    }
 
 }
