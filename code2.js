c24a25b15885fe935b5c2b81189dad04486dde4c
Arik Talmon <Ariktalmon@students.berkeley.net>
2017-02-21 11:06:32 -0800

    commit

--- code2.js
+++ code2.js
@@ -1,8 +1,8 @@
+var possibleArray = [];
 
 function run() {
 
-    //use myVars instead?
-    var possibleArray = [];
+    possibleArray = [];
 
     var coast = document.getElementById("coast").value;
     if (coast == "false") {
@@ -29,9 +29,40 @@ function run() {
     }
 
     if (possibleArray.length == 0) {
-        console.log("Im sorry, but we came up with no results that match your specifications.")
+        run2();
     } else {
         console.log(possibleArray);
     }
 
+}
+
+function run2() {
+
+    console.log("hi");
+
+    possibleArray = [];
+
+    var coast = document.getElementById("coast").value;
+    if (coast == "false") {
+        coast = (coast == "false");
+    } else {
+        coast = (coast == "true");
+    }
+    var pol = document.getElementById("politics").value;
+    var weather = document.getElementById("weather").value;
+    var city = document.getElementById("bigcity").value;
+    if (city == "false") {
+        city = (city == "false");
+    } else {
+        city = (city == "true");
+    }
+    var diver = document.getElementById("diversity").value;
+
+    for (var p = 0; p < stateArray.length; p++) {
+// diversity and weather removed
+        if (stateArray[p].coast == coast && stateArray[p].politics == pol && stateArray[p].bigcity == city) {
+            possibleArray = possibleArray + stateArray[p].name;
+        }
+
+    }
 }
